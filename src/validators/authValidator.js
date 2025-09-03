4fb1dac8f4bb633cee76eaa65a650fb5f2e6a21f
fajar buyung <fajarbuyung65@gmail.com>
2025-09-03 12:03:36 +0700

    validator input

--- src/validators/authValidator.js
+++ src/validators/authValidator.js
@@ -3,16 +3,22 @@ const { body } = require('express-validator');
 const authValidator = {
   registerValidator: [
     body('name')
+      .exists().withMessage('Name wajib diisi')
+      .bail()
       .isString().withMessage('Name harus berupa teks')
       .notEmpty().withMessage('Name wajib diisi')
       .isLength({ min: 3 }).withMessage('Name minimal 3 karakter'),
 
     body('username')
+      .exists().withMessage('Username wajib diisi')
+      .bail()
       .isString().withMessage('Username harus berupa teks')
       .notEmpty().withMessage('Username wajib diisi')
       .isAlphanumeric().withMessage('Username hanya boleh huruf dan angka'),
 
     body('password')
+      .exists().withMessage('Password wajib diisi')
+      .bail()
       .isString().withMessage('Password harus berupa teks')
       .notEmpty().withMessage('Password wajib diisi')
       .isLength({ min: 6 }).withMessage('Password minimal 6 karakter')
@@ -20,10 +26,14 @@ const authValidator = {
 
   loginValidator: [
     body('username')
+      .exists().withMessage('Username wajib diisi')
+      .bail()
       .isString().withMessage('Username harus berupa teks')
       .notEmpty().withMessage('Username wajib diisi'),
 
     body('password')
+      .exists().withMessage('Username wajib diisi')
+      .bail()
       .isString().withMessage('Password harus berupa teks')
       .notEmpty().withMessage('Password wajib diisi'),
   ],
--- src/validators/blogValidator.js
+++ src/validators/blogValidator.js
@@ -3,15 +3,21 @@ const { body } = require('express-validator');
 const blogValidator = {
   create: [
     body('title')
+      .exists().withMessage('Judul wajib diisi')
+      .bail()
       .notEmpty().withMessage('Judul wajib diisi')
       .isString().withMessage('Judul harus berupa teks')
       .isLength({ min: 5 }).withMessage('Judul minimal terdiri dari 5 karakter'),
 
     body('date')
+      .exists().withMessage('Tanggal wajib diisi')
+      .bail()
       .notEmpty().withMessage('Tanggal wajib diisi')
       .isISO8601().withMessage('Format tanggal tidak valid (gunakan format ISO 8601, contoh: 2025-08-01)'),
 
     body('description')
+      .exists().withMessage('Deskripsi wajib diisi')
+      .bail()
       .notEmpty().withMessage('Deskripsi wajib diisi')      
       .isString().withMessage('Deskripsi harus berupa teks')
       .isLength({ min: 10 }).withMessage('Deskripsi minimal terdiri dari 10 karakter'),
@@ -19,15 +25,21 @@ const blogValidator = {
 
   update: [
     body('title')
+      .exists().withMessage('Title wajib diisi')
+      .bail()
       .optional()
       .isString().withMessage('Judul harus berupa teks')
       .isLength({ min: 5 }).withMessage('Judul minimal terdiri dari 5 karakter'),
 
     body('date')
+      .exists().withMessage('Date wajib diisi')
+      .bail()
       .optional()
       .isISO8601().withMessage('Format tanggal tidak valid (gunakan format ISO 8601, contoh: 2025-08-01)'),
 
     body('description')
+      .exists().withMessage('Deskripsi wajib diisi')
+      .bail()
       .optional()
       .isString().withMessage('Deskripsi harus berupa teks')
       .isLength({ min: 10 }).withMessage('Deskripsi minimal terdiri dari 10 karakter'),
